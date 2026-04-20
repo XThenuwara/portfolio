aee83324eb3d68d2cd5801723b05d18cfff0db7c
Yasas Hansaka Thenuwara <xthenuwara@gmail.com>
2026-04-20 09:31:11 +0530

    Updated to nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     output: 'export',
+    turbopack: {},
     basePath: process.env.NODE_ENV === 'development' ? '/portfolio' : '/portfolio',
     assetPrefix: process.env.NODE_ENV === 'development' ? '/portfolio' : './',
     webpack: (config, { dev, isServer }) => {
